5839ef45f25cfc2d87e1aec8e083e93d0e28911b
Liberosis <buithingocgiau1966@gmail.com>
2024-10-28 14:17:15 +0700

    refactor(terraform)

--- Terraform/modules/vpc/outputs.tf
+++ Terraform/modules/vpc/outputs.tf
@@ -13,3 +13,27 @@ output "private_subnet_id" {
 output "internet_gateway_id" {
   value = aws_internet_gateway.igw.id
 }
+
+output "public_subnet_tags"{
+  value = aws_subnet.public_subnet.tags
+}
+
+output "private_subnet_tags"{
+  value = aws_subnet.private_subnet.tags
+}
+
+output "internet_gateway_tags"{
+  value = aws_internet_gateway.igw.tags
+}
+
+output "vpc_cidr_block"{
+  value = aws_vpc.main_vpc.cidr_block
+}
+
+output "public_subnet_cidr_block"{
+  value = aws_subnet.public_subnet.cidr_block
+}
+
+output "private_subnet_cidr_block"{
+  value = aws_subnet.private_subnet.cidr_block
+}
